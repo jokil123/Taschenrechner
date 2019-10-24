3281a0a0c773cbfaac5e6f0d0c94b6cfa1188555
jokil123 <46920345+jokil123@users.noreply.github.com>
2019-10-24 09:35:31 +0200

    Update 0.2
    -theme changer script is now functional
    -added theme test text
    -minor theme css changes

--- script/theme_script.js
+++ script/theme_script.js
@@ -1,3 +1,49 @@
+/*
 document.getElementById("changeTheme").addEventListener("click"), function() {
     document.body.classList.toggle("dark-theme");
+}
+*/
+
+var style = 0
+var styleName = ""
+
+
+style = parseInt(localStorage.getItem("style"));
+if (isNaN(style) == true) {
+    localStorage.setItem("style", 0);
+}
+
+document.getElementById('themeSheet').href = numName();
+
+function changeTheme() {
+    document.getElementById('themeSheet').href = numName();
+}
+
+function numName() {
+    switch (style) {
+        case 0:
+            styleName = "style/themes/light_theme.css"
+            localStorage.setItem("style", 0);
+            style += 1;
+            break;
+
+        case 1:
+            styleName = "style/themes/dark_theme.css"
+            localStorage.setItem("style", 1);
+            style += 1;
+            break;
+        
+        case 2:
+            style = 0
+            numName()
+            break;
+
+        default:
+                styleName = "style/themes/light_theme.css"
+                localStorage.setItem("style", 0);
+                style += 1;
+            break;
+    }
+
+    return styleName;
 }
