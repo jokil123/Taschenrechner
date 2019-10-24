c8de9af6148e862e0865bddc6b64070e4127b919
jokil123 <46920345+jokil123@users.noreply.github.com>
2019-10-24 23:06:02 +0200

    Update 0.3
    -fixed alignment of text bot
    -simplified calculation script
    -finished Dark theme
    -finished White theme

--- script/calculation_script.js
+++ script/calculation_script.js
@@ -7,7 +7,7 @@ var maxInputLenght = ""
 
 var checkIllegal = ""
 var illegal = ["&pi;", "e", "²", "³"]
-var legal = ["3.1415", "2.7182", "**2", "**3"]
+var legal = ["3.14159265359", "2.71828182846", "**2", "**3"]
 var illegalPosition = 0
 var illegalLength = 0
 
@@ -16,124 +16,13 @@ var calcOutput = ""
 var cursorPosition = 0
 var cursorPositionOld = 0
 
-function buttonOne() {
-	calcInput += 1
-	//buttonRight();
+
+function input(button) {
+	calcInput += button
 	shortInput();
 	document.getElementById("output").innerHTML = shortDisplay;
 }
 
-function buttonTwo() {
-	calcInput += 2
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonThree() {
-	calcInput += 3
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonFour() {
-	calcInput += 4
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonFive() {
-	calcInput += 5
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonSix() {
-	calcInput += 6
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonSeven() {
-	calcInput += 7
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonEight() {
-	calcInput += 8
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonNine() {
-	calcInput += 9
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonZero() {
-	calcInput += 0
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonComma() {
-	calcInput += "."
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonPlus() {
-	calcInput += "+"
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonMinus() {
-	calcInput += "-"
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonMultiply() {
-	calcInput += "*"
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonDivide() {
-	calcInput += "/"
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonOpenBracket() {
-	calcInput += "("
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonCloseBracket() {
-	calcInput += ")"
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
 
 function buttonPi() {
 	calcInput += "&pi;"
@@ -142,26 +31,6 @@ function buttonPi() {
 	document.getElementById("output").innerHTML = shortDisplay;
 }
 
-function buttonEuler() {
-	calcInput += "e"
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonSquare() {
-	calcInput += "²"
-	//buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
-
-function buttonCube() {
-	calcInput += "³"
-	////buttonRight();
-	shortInput();
-	document.getElementById("output").innerHTML = shortDisplay;
-}
 
 
 function buttonBack() {
